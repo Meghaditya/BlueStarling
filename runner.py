1c97f1dee440f477ba2789b2dac17cd0b051c3da
Meghaditya Roy Chaudhury <aditya.roychaudhury@gmail.com>
2016-09-04 01:20:54 +0530

    adding trends experimentation

--- runner.py
+++ runner.py
@@ -1,8 +1,11 @@
+import time
+from time import gmtime, strftime
 from utils import custom_prompt
 from utils import fetch_twitter_credential
 from twython import Twython
 from simple_retweet import simple_iterative_retweet
 from profile_cleaner import destroy_all_favorites
+from trends import get_trends_from_places
 
 (APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET) = fetch_twitter_credential()
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
@@ -10,6 +13,7 @@ twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 mode_text = 'Enter mode :\n' + \
 			'1. Simple Iterative Retweet.\n' + \
 			'2. Clean Profile\n' + \
+			'3. Get Hourly Trends in India\n' + \
 			'Your Choice : '
 
 mode = int(custom_prompt(mode_text))
@@ -20,5 +24,15 @@ if mode == 1:
 elif mode == 2:
 	screen_name = custom_prompt('Enter screen name : ')
 	reset_profile(twitter, screen_name)
+elif mode == 3:
+	while True:
+		print('Getting Hourly trend in India')
+		print(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
+		print('-------------')
+		trends = get_trends_from_places(twitter, ['India'], [])
+		for trend in trends:
+			print(trend)
+		print('-------------')
+		time.sleep(60*60)
 else:
 	print('wrong input')
---
+++ trends.py
@@ -0,0 +1,81 @@
+import time
+import operator
+
+# Get all the places with(out) the filters
+#
+# Params:
+# Twitter - the Twython object
+# country_names - the list of country names to filter with
+# place_names - the list of place names to filter with
+
+def get_places(twitter, country_names = [], place_names = []):
+	
+	# All the available places for trends query
+	places = twitter.get_available_trends()
+	places_of_interest = 0
+	places_list = []
+
+	for place in places:
+		#print(place['country'])
+		#print(place['country'] in country_names)	
+
+		if ((len(country_names)>0) and \
+			(place['country'] in country_names)) :
+			places_of_interest += 1
+			print('Added to places ', place['name'])
+			places_list.append(place)
+		
+		if ((len(place_names)>0) and \
+			  (place['name'] in place_names)) :
+			places_of_interest += 1
+			print('Added to places ', place['name'])
+			places_list.append(place)
+		
+		if ((not country_names) and \
+			(not place_names)) :
+			places_of_interest += 1
+			print('Added to places ', place['name'])
+			places_list.append(place)
+
+	return places_list
+
+
+# Get all the trends after finding out the places
+#
+# Params:
+# Twitter - the Twython object
+# country_names - the list of country names to filter with
+# place_names - the list of place names to filter with
+
+def get_trends_from_places(twitter, country_names = [], place_names = []):
+
+	return get_trends(twitter, get_places(twitter, country_names, place_names))
+	
+
+# Get the trends from list of places
+#
+# Params:
+# places_list - the list of places
+
+def get_trends(twitter, places_list):
+
+	trends_of_ineterest = dict([])
+
+	for place in places_list:
+
+		trends_for_place = twitter.get_place_trends(id=place['woeid'])
+		print('Got the trends for ', place['name'])
+		
+		for trend in trends_for_place[0]['trends']:
+		
+			if trend['name'] in trends_of_ineterest:
+				oldCount = trends_of_ineterest[trend['name']]
+				trends_of_ineterest[trend['name']] = oldCount + 1
+		
+			else:
+				trends_of_ineterest[trend['name']] = 1
+		time.sleep(60)
+
+	return reversed(\
+		sorted(trends_of_ineterest.items(), \
+			key=operator.itemgetter(1)))
